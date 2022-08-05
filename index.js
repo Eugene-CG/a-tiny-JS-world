6bb4ce2ff72003786296db719cdafd0cdac90857
Eugene-CG <evhenisssss@gmail.com>
2022-08-05 20:26:19 +0300

    fix minor problem

--- index.js
+++ index.js
@@ -8,7 +8,6 @@
 // ======== OBJECTS DEFINITIONS ========
 // Define your objects here
 
-
 // ======== OUTPUT ========
 /* Use print(message) for output.
    Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
@@ -17,6 +16,63 @@
    However, please, REFRAIN from improving visuals at least until your code is reviewed
    so code reviewers might focus on a single file that is index.js.
    */
+const cat = {
+  species: "cat",
+  name: "Eugene",
+  gender: "male",
+  legs: 4,
+  hands: 0,
+  saying: "Hey Fellas!",
+};
+const dog = {
+  species: "dog",
+  name: "Toby",
+  gender: "male",
+  legs: 4,
+  hands: 0,
+  saying: "woof-woof!",
+};
+const man = {
+  species: "man",
+  name: "John",
+  gender: "male",
+  legs: 2,
+  hands: 2,
+  saying: "I am a man",
+};
+const woman = {
+  species: "dog",
+  name: "Yennefer",
+  gender: "female",
+  legs: 2,
+  hands: 2,
+  saying: "Nice to meet you",
+};
+const catWoman = {
+  species: "catwoman",
+  name: "Violet Flower",
+  gender: "female",
+  legs: 2,
+  hands: 2,
+};
+Object.setPrototypeOf(catWoman, cat);
+
+const inhabitants = [cat, dog, man, woman, catWoman];
+const inhabitantsKeys = [
+  "species",
+  "name",
+  "gender",
+  "legs",
+  "hands",
+  "saying",
+];
+inhabitants.forEach((obj) => {
+  print(
+    inhabitantsKeys.reduce((accumulator, prop) => {
+      return (accumulator += obj[prop] + "; ");
+    }, "")
+  );
+});
 
 /* Print examples:
    print('ABC');
@@ -27,5 +83,3 @@
    print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
    print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
    */
-
-
